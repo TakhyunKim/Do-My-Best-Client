dab4f99adc03476a5e91d287ef0cb7415d9ee6f3
Takhyun Kim <kimkih1218@gmail.com>
2022-01-31 23:52:52 +0900

    feat: add LabelButton component

---
+++ src/components/UI/molecules/LabelButton/LabelButton.types.ts
@@ -0,0 +1,11 @@
+import { CSSProperties } from 'react';
+
+interface LabelButtonProps {
+	buttonText: string;
+	labelText: string;
+	buttonStyle?: CSSProperties;
+	labelStyle?: CSSProperties;
+	wrapperStyle?: CSSProperties;
+}
+
+export type { LabelButtonProps };
