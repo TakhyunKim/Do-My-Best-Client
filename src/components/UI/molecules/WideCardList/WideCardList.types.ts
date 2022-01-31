6ff909ed322256bcb1308748afb920c269fb2ff0
Takhyun Kim <kimkih1218@gmail.com>
2022-01-31 23:54:43 +0900

    feat: add WideCardList component

---
+++ src/components/UI/molecules/WideCardList/WideCardList.types.ts
@@ -0,0 +1,9 @@
+import { CSSProperties } from 'react';
+
+import { AdvantageInformation } from '@common/types';
+
+interface WideCardListProps {
+	advantageInformations: AdvantageInformation[];
+}
+
+export type { WideCardListProps };
